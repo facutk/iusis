722856ea67452c9966eb3b36a8f41709d814f42b
facu.tk <facu.tk@gmail.com>
2016-08-26 17:35:04 -0300

    image compression done

--- index.js
+++ index.js
@@ -1,69 +1,37 @@
-/*
-var phantom = require('phantom');
 
-phantom.create(function (ph) {
-    ph.createPage(function (page) {
-        page.open("http://www.google.com", function (status) {
-
-            var paperConfig = {
-                format: 'A4',
-                orientation: 'portrait',
-                border: '1cm',
-                header: {
-                    height: '1cm',
-                    contents: ph.callback(function(pageNum, numPages) {
-                        return '<h1>My Custom Header</h1>';
-                    })
-                },
-                footer: {
-                    height: '1cm',
-                    contents: ph.callback(function(pageNum, numPages) {
-                        return '<p>Page ' + pageNum + ' / ' + numPages + '</p>';
-                    })
-                }
-            };
-
-            page.set('paperSize', paperConfig, function() {
-                // render to pdf
-                page.render('page.pdf', function() {
-                    page.close();
-                    ph.exit();
-                });
-            });
-        });
-    });
-});
-*/
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
 
 var phantom = require("phantom");
-var _ph, _page, _outObj;
 
-phantom.create().then(ph => {
-    _ph = ph;
-    return _ph.createPage();
-}).then(page => {
-    _page = page;
-    _page.paperSize = {
-        format: 'A4',
-        orientation: 'portrait',
-        margin: '15cm'
-    };
-    _page.viewportSize = {
-        width: 100,
-        height: 100,
-    };
-    _page.property('paperSize', {format: 'A4', orientation: 'portrait', margin: '1cm'});
-    _page.property('paperSize').then( function(content) {
-        console.log( content );
-    });
-    //return _page.open('https://secret-sierra-35258.herokuapp.com/');
-    return _page.open('https://secret-sierra-35258.herokuapp.com/');
-}).then(status => {
-    console.log(status);
-    _page.render('page.pdf');
-    return _page.property('content')
-}).then(content => {
-    console.log(content);
-    _page.close();
-    _ph.exit();
-}).catch(e => console.log(e));
+imagemin(['sample/*.{jpg,png}'], 'sample/compressed', {
+    plugins: [
+        imageminMozjpeg(),
+        imageminPngquant({quality: '65-80'})
+    ]
+}).then(files => {
+    console.log(files);
+    //=> [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
+
+    var _ph, _page, _outObj;
+    phantom.create().then(ph => {
+        _ph = ph;
+        return _ph.createPage();
+    }).then(page => {
+        _page = page;
+        _page.property('paperSize', {format: 'A4', orientation: 'portrait', margin: '0cm'});
+        _page.property('viewportSize', {width: 768, height: 1024});
+        return _page.open('file:///C:/Users/f.tkaczyszyn/Desktop/iusis/sample/compressed/index.html');
+
+    }).then(status => {
+        console.log(status);
+        //_page.render('page.png');
+        _page.render('page.pdf');
+        return _page.property('content')
+    }).then(content => {
+        console.log(content);
+        _page.close();
+        _ph.exit();
+    }).catch(e => console.log(e));
+});
