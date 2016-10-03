00ccd752ca302487462b54157aa14f3963da8480
facu.tk <facu.tk@gmail.com>
2016-10-03 18:11:57 -0300

    add tree-view

--- src/components/PdfComposer.jsx
+++ src/components/PdfComposer.jsx
@@ -5,6 +5,146 @@ import { Sortable } from 'react-sortable';
 import { dispatch } from 'redux';
 import { connect } from 'react-redux';
 import './PdfComposer.scss';
+const cx = require('classnames');
+
+const Tree = require('react-ui-tree');
+
+var TreeApp = React.createClass({
+  getInitialState() {
+    return {
+      active: null,
+      tree:
+      {
+        module: 'PDF',
+        children: [{
+          module: 'dist',
+          collapsed: true,
+          children: [{
+            module: 'node.js',
+            leaf: true
+          }, {
+            module: 'react-ui-tree.css',
+            blob: 'blob://localhost',
+            leaf: true
+          }, {
+            module: 'react-ui-tree.js',
+            leaf: true
+          }, {
+            module: 'tree.js',
+            leaf: true
+          }]
+        }, {
+          module: 'example',
+          children: [{
+            module: 'app.js',
+            leaf: true
+          }, {
+            module: 'app.less',
+            leaf: true
+          }, {
+            module: 'index.html',
+            leaf: true
+          }]
+        }, {
+          module: 'lib',
+          children: [{
+            module: 'node.js',
+            leaf: true
+          }, {
+            module: 'react-ui-tree.js',
+            leaf: true
+          }, {
+            module: 'react-ui-tree.less',
+            leaf: true
+          }, {
+            module: 'tree.js',
+            leaf: true
+          }]
+        }, {
+          module: '.gitiignore',
+          leaf: true
+        }, {
+          module: 'index.js',
+          leaf: true
+        }, {
+          module: 'LICENSE',
+          leaf: true
+        }, {
+          module: 'Makefile',
+          leaf: true
+        }, {
+          module: 'package.json',
+          leaf: true
+        }, {
+          module: 'README.md',
+          leaf: true
+        }, {
+          module: 'webpack.config.js',
+          leaf: true
+        }]
+      }
+
+    };
+  },
+
+    renderNode(node) {
+        return (
+            <span
+                className={cx('node', {
+                'is-active': node === this.state.active        })}
+                onClick={this.onClickNode.bind(null, node)}
+            >
+                {node.module}
+            </span>
+        );
+    },
+
+    onClickNode(node) {
+        this.setState({
+            active: node
+        });
+    },
+
+    render() {
+        return (
+            <div className="app">
+                <div className="tree">
+                    <Tree
+                        paddingLeft={20}
+                        tree={this.state.tree}
+                        onChange={this.handleChange}
+                        isNodeCollapsed={this.isNodeCollapsed}
+                        renderNode={this.renderNode}
+                    />
+                </div>
+
+                <div className="inspector">
+                    <button onClick={this.updateTree}>update tree</button>
+                    <pre>
+                        {JSON.stringify(this.state.tree, null, '  ')}
+                    </pre>
+                </div>
+
+            </div>
+        );
+    },
+
+    handleChange(tree) {
+        this.setState({
+            tree: tree
+        });
+    },
+
+    updateTree() {
+        var tree = this.state.tree;
+        tree.children.push({module: 'test'});
+        this.setState({
+            tree: tree
+        });
+    }
+
+});
+
 
 const PreviewContent = function(props) {
     return (
@@ -277,15 +417,14 @@ class Composer extends React.Component {
     }
 
     render() {
-        var divStyle = {
-            backgroundColor: this.state.hovering?'red':'white'
-        };
+
+        //<TreeApp />
         return (
             <div className="pdf-composer">
                 <div className="ui centered card">
                     <div className="ui">
                         <Dropzone
-                            className={"dropzone " + 'active'}
+                            className="dropzone"
                             onDrop={this.onFileDrop}>
                             <div>Hacé click o arrastrá archivos</div>
                             <ul>
